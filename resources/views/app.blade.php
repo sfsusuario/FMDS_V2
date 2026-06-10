70f375a1f5229c92bda484e278c69a4a66ba79dd
samael.fierro <samael.fierro@n5now.com>
2026-06-09 21:35:51 -0500

    Remove assets

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="robots" content="index, follow">
         <link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml">
-        <link rel="icon" type="image/x-icon" href="/favicon.ico">
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+        <link rel="alternate icon" type="image/x-icon" href="/favicon.ico">
         <title inertia>{{ config('app.name', 'Fundación Mesa del Señor') }}</title>
         @routes
         @viteReactRefresh
